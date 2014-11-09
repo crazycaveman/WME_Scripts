4e1d112424c278ae24ba30dcbd5bd4d46826b3c8
Joe <crazycaveman@users.noreply.github.com>
2014-11-09 05:59:56 -0500

    Update Validator_SC_localization.user.js

--- Validator_SC_localization.user.js
+++ Validator_SC_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for South Carolina
 // @namespace           https://greasyfork.org/users/6605
-// @version             1.1.4.1.1
+// @version             1.1.4.1.2
 // @author              crazycaveman
 // @description         This script localizes WME Validator for South Carolina, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -99,7 +99,7 @@ window.WME_Validator_United_States = {
     "problemEN": "Secondary state roads should follow the format S-##-XXX",
     "solutionEN": "Change road name to only be S-##-XXX, where XX is the county number",
     "template": "#${street}@#${altStreet[0]}@#${altStreet[1]}@#${altStreet[2]}@#${altStreet[3]}@#${altStreet[4]}@#${altStreet[5]}@#${altStreet[6]}@#${altStreet[7]}@#${altStreet[8]}@#${altStreet[9]}@",
-    "regexp": "/#(?:.* S-[0-9]{1,2}-[0-9]+|[A-Z][ -][0-9]+)@/i"
+    "regexp": "/#.* S-[0-9]{1,2}-[0-9]+@/i"
   },
   "135.solutionLink": "W:South_Carolina#Naming"
 };
