875b14f207383663e5ec5dfb36b5818915347517
xanderb-waze <xander@louisvillehighlands.net>
2015-03-05 22:10:05 -0500

    Update Validator_KY_localization.user.js

--- Validator_KY_localization.user.js
+++ Validator_KY_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Kentucky
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.1
+// @version             1.1.1.2
 // @author              xanderb
 // @description         This script localizes WME Validator for Kentucky, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -46,4 +46,12 @@ window.WME_Validator_United_States = {
     "regexp": "/1:7:([0-4]?[0-9])$/"
   },
   "130.solutionLink": "W:Best_map_editing_practice#Parking_Lots"
+  "131.enabled": true,
+  "131.params": {
+    "titleEN": "Saint not Street TTS",
+    "problemEN": "Streets that start with St result in TTS reading Street",
+    "solutionEN": "Add a period after  St and the beginning of the street name",
+    "template": "#${street}#${altStreet[#]}#",
+    "regexp": "/^St /"
+  }
 };
