dceb7a2d7381839df2cd61c7e9148ef121134f01
xanderb-waze <xander@louisvillehighlands.net>
2015-03-05 22:02:42 -0500

    Update Validator_KY_localization.user.js

--- Validator_KY_localization.user.js
+++ Validator_KY_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Kentucky
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.6
+// @version             1.1.1.1
 // @author              xanderb
 // @description         This script localizes WME Validator for Kentucky, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -45,39 +45,5 @@ window.WME_Validator_United_States = {
     "template": "${deadEnd}:${typeRank}:${length}",
     "regexp": "/1:7:([0-4]?[0-9])$/"
   },
-  "130.solutionLink": "W:Best_map_editing_practice#Parking_Lots",
-  "131.enabled": true,
-  "131.params": {
-    "titleEN": "Wrong name for state highway",
-    "problemEN": "All state highways should be named KY-xxx in Kentucky",
-    "solutionEN": "Change name to KY-xxx if it is truly a state highway",
-    "template": "${state}:#${street}#${altStreet[#]}#",
-    "regexp": "/Kentucky:State Hwy|SR-|SH-|State Rd|Ky-|Tn-|In-|KY=|IN-|TN-/"
-  },
-  "131.solutionLink": "W:Kentucky/Test#State_Highway_Renaming",
-  "132.enabled": true,
-  "132.params": {
-    "titleEN": "Wrong name for City or County street",
-    "problemEN": "CS and Cs are not read correctly by TTS or County Hwy Name is wrong",
-    "solutionEN": "Check sources and change the street's name",
-    "template": "${state}:#${street}#${altStreet[#]}#",
-    "regexp": "/Kentucky:Cs-|CS-|County Hwy|County Rd|Cr-/"
-  },
-  "133.enabled": true,
-  "133.params": {
-    "titleEN": "Saint not Street TTS",
-    "problemEN": "Streets that incorrectly begin with St results with TTS reading Street",
-    "solutionEN": "Add a period after St at the beginning of the street name",
-    "template": "#${street}#${altStreet[#]}#",
-    "regexp": "/^St /"
-  },
-  "133.solutionLink": "W:Abbreviations_and_acronyms#Standard_suffix_abbreviations"
-  "134.enabled"= true,
-  "134.params": {
-    "titleEN": "Wrong State",
-    "problemEN": "This state is not Kentucky",
-    "solutionEN": "If you are editing in Kentucky change the state name",
-    "template": "$(state)",
-    "regexp": "/!Kentucky/"
-  }
+  "130.solutionLink": "W:Best_map_editing_practice#Parking_Lots"
 };
