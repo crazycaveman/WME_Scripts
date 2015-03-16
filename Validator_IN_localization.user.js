0d5a849b6f55679ce6fbd58eddbda1daf97935dc
xanderb-waze <xander@louisvillehighlands.net>
2015-03-16 19:42:30 -0400

    Update Validator_IN_localization.user.js
    Corrected state name

--- Validator_IN_localization.user.js
+++ Validator_IN_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Indiana
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.4
+// @version             1.1.1.5
 // @author              xanderb
 // @description         This script localizes WME Validator for Indiana, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-03-12",
+  ".updated": "2015-03-16",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -49,7 +49,7 @@ window.WME_Validator_United_States = {
   "129.enabled": true,
   "129.params": {
     "titleEN": "Incorrect State Highway",
-    "problemEN": "Kentucky uses IN for numbered highway names",
+    "problemEN": "Indiana uses IN for numbered highway names",
     "solutionEN": "Rename the Street or Alt Street",
     "template": "${state}:${street}#${altStreet[#]}",
     "regexp": "/Indiana:.*(In Hwy |State Hwy |In-|SR-|SH-|State Rd|IN=|State Rte)/"
