7891cf7632538798648d11ef4c7a9256a1f9eb8f
xanderb-waze <xander@louisvillehighlands.net>
2015-03-29 23:00:23 -0400

    Update Validator_KY_localization.user.js
    Temp downlock of interstates to L4

--- Validator_KY_localization.user.js
+++ Validator_KY_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Kentucky
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.24
+// @version             1.1.1.25
 // @author              xanderb
 // @description         This script localizes WME Validator for Kentucky, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -106,7 +106,7 @@ window.WME_Validator_United_States = {
     "problemEN": "Minimum Lock Level in Kentucky follows 54321 standard",
     "solutionEN": "Make sure it isn't unlocked for someone at a lower rank to work on and lock it",
     "template": "${typeRank}:${lock}",
-    "regexp": "!/^(15:[5-6]|14:[4-6]|13:[3-6]|12:[2-6]|11:[2-6]|10:[1-6]|2:[2-6]|[1,3-9]:[1-6])$/"
+    "regexp": "!/^(15:[4-6]|14:[4-6]|13:[3-6]|12:[2-6]|11:[2-6]|10:[1-6]|2:[2-6]|[1,3-9]:[1-6])$/"
   },
   "135.solutionLink": "W:Kentucky/Test"
 };
