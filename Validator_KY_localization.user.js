248055fb1113dd02d82e48e2257e9390e7ebe51a
xanderb-waze <xander@louisvillehighlands.net>
2015-04-29 22:00:09 -0400

    Update Validator_KY_localization.user.js

--- Validator_KY_localization.user.js
+++ Validator_KY_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Kentucky
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.34
+// @version             1.1.1.35
 // @author              xanderb
 // @description         This script localizes WME Validator for Kentucky, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-04-28",
+  ".updated": "2015-04-29",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -110,6 +110,15 @@ window.WME_Validator_United_States = {
     "regexp": "!/^(15:[4-6]|14:[4-6]|13:[3-6]|12:[2-6]|11:[2-6]|10:[1-6]|2:[2-6]|[1,3-9]:[1-6])$/"
   },
   "135.solutionLink": "W:Kentucky/Test",
+  "136.enabled": true,
+  "136.params": {
+    "titleEN": "Wrong road type",
+    "problemEN": "All US Highways should be at least Major Highway (except BUS, SPUR, LOOP)",
+    "solutionEN": "Change the road type to Major Highway",
+    "template": "${typeRank}:#${street}@#${altStreet[@#]}@",
+    "regexp": "/^[1-9][^245]?:.*#(US Hwy |US-)[0-9]+( ALT| BYP| CONN| TRUCK| Scenic| [NSWE])*@/i"
+  },
+  "136.solutionLink": "W:Road_types/USA#Major_Highway",
   //Freeway lock
   "150.enabled": true,
   "150.params": {
