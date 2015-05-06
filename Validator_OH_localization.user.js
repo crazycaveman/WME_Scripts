b43b57f74ecfaa39548621846252e1717ef6cba4
xanderb-waze <xander@louisvillehighlands.net>
2015-05-06 16:30:18 -0400

    Update Validator_OH_localization.user.js

--- Validator_OH_localization.user.js
+++ Validator_OH_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Ohio
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.13
+// @version             1.1.1.14
 // @author              xanderb
 // @description         This script localizes WME Validator for Ohio, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-04-29",
+  ".updated": "2015-05-06",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -58,10 +58,10 @@ window.WME_Validator_United_States = {
   "130.enabled": true,
   "130.params": {
     "titleEN": "Short Parking Lot",
-    "problemEN": "Dead End Parking Lot is less that 50 meters",
+    "problemEN": "Dead End Parking Lot is less that 40 meters",
     "solutionEN": "Does it really need to be there?",
     "template": "${deadEnd}:${typeRank}:${length}",
-    "regexp": "/1:7:([0-4]?[0-9])$/"
+    "regexp": "/1:7:([0-3]?[0-9])$/"
   },
   "130.solutionLink": "W:Best_map_editing_practice#Parking_Lots",
   "131.enabled": true,
@@ -79,7 +79,7 @@ window.WME_Validator_United_States = {
     "problemEN": "CS and Cs are not read correctly by TTS or County Hwy Name is wrong",
     "solutionEN": "Check sources and change the street's name",
     "template": "${state}:${street}#${altStreet[#]}",
-    "regexp": "/Ohio:.*(Cs-|CS-|County Hwy|County Rd|Cr-|Co Rd|Ch-)/"
+    "regexp": "/Ohio:.*(Cs-|CS-|County Hwy|County Rd|Cr-|Co Rd|Ch-|T-|C-)/"
   },
   "132.solutionLink": "W:Highway_naming/USA",
   "133.enabled": true,
