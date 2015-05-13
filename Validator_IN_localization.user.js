78f0e277d9532284b394141e2bc321d4c03cfa09
xanderb-waze <xander@louisvillehighlands.net>
2015-05-12 22:52:26 -0400

    Update Validator_IN_localization.user.js
    dup

--- Validator_IN_localization.user.js
+++ Validator_IN_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Indiana
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.17
+// @version             1.1.1.18
 // @author              xanderb
 // @description         This script localizes WME Validator for Indiana, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -90,15 +90,6 @@ window.WME_Validator_United_States = {
     "template": "${typeRank}:${city}",
     "regexp": "/15:.+$/"
   },
-  "134.enabled": true,
-  "134.params": {
-    "titleEN": "Wrong road type",
-    "problemEN": "All US Highways should be at least Major Highway (except BUS, SPUR, LOOP)",
-    "solutionEN": "Change the road type to Major Highway",
-    "template": "${typeRank}:#${street}@#${altStreet[@#]}@",
-    "regexp": "/^[1-9][^245]?:.*#(US Hwy |US-)[0-9]+( ALT| BYP| CONN| TRUCK| Scenic| [NSWE])*@/i"
-  },
-  "134.solutionLink": "W:Road_types/USA#Major_Highway",
   "135.enabled": true,
   "135.params": {
     "titleEN": "Wrong road type (major)",
