f676ef75413b1942601946692686cdcce623b288
xanderb-waze <xander@louisvillehighlands.net>
2015-05-13 11:49:44 -0400

    Update Validator_OH_localization.user.js
    Oops

--- Validator_OH_localization.user.js
+++ Validator_OH_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Ohio
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.16
+// @version             1.1.1.17
 // @author              xanderb
 // @description         This script localizes WME Validator for Ohio, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-05-12",
+  ".updated": "2015-05-13",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -107,7 +107,7 @@ window.WME_Validator_United_States = {
     "template": "${typeRank}:#${street}#${altStreet[#]}",
     "regexp": "/[1-9][0,6-9]:.*(CH-|CR-)/"
   },
-  "135.solutionLink": "W:Road_types/USA#Quick_reference_chart",,
+  "135.solutionLink": "W:Road_types/USA#Quick_reference_chart",
   "136.enabled": true,
   "136.params": {
     "titleEN": "Wrong road type (major)",
