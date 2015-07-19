e34510e2ada83ba786160acce783931c16d779a0
xanderb-waze <xander@louisvillehighlands.net>
2015-07-19 08:00:48 -0400

    Create Validator_MA_localization.user.js
    Initial Save

---
+++ Validator_MA_localization.user.js
@@ -0,0 +1,163 @@
+// ==UserScript==
+// @name                WME Validator Localization for Massachusetts
+// @namespace           https://greasyfork.org/en/users/9555
+// @version             1.1.1.10
+// @author              xanderb
+// @description         This script localizes WME Validator for Massachusetts, USA. You also need main package (WME Validator) installed.
+// @match               https://editor-beta.waze.com/*editor/*
+// @match               https://www.waze.com/*editor/*
+// @grant               none
+// @run-at              document-start
+// ==/UserScript==
+//
+/*
+  See Settings->About->Available checks for complete list of checks and their params.
+  Examples:
+  Enable #170 "Lowercase street name" but allow lowercase "exit" and "to":
+    "170.enabled": true,
+    "170.params": {
+        "regexp": "/^((exit|to) )?[a-z]/",
+    "},
+  Enable #130 "Custom check" to find a dot in street names, but allow dots at Ramps:
+    "130.enabled": true,
+    "130.params": {
+        "titleEN": "Street name with a dot",
+        "problemEN": "There is a dot in the street name (excluding Ramps)",
+        "solutionEN": "Expand the abbreviation or remove the dot",
+        "template": "${type}:${street}",
+        "regexp": "D/^[^4][0-9]?:.*\\./",
+    },
+    *Note: use D at the beginning of RegExp to enable debugging on JS console.
+    *Note: do not forget to escape backslashes in strings, i.e. use "\\" instead of "\".
+*/
+
+window.WME_Validator_United_States = {
+  ".country": "United States",
+  ".codeISO": "US",
+  ".author": "xanderb",
+  ".updated": "2015-07-19",
+  ".link": "TODO: ",
+  "130.enabled": false,
+  "130.params": {
+    "titleEN": "Short Parking Lot",
+    "problemEN": "Dead End Parking Lot is less that 30 meters",
+    "solutionEN": "Does it really need to be there?",
+    "template": "${deadEnd}:${typeRank}:${length}",
+    "regexp": "/1:7:([0-2]?[0-9])$/"
+  },
+  "130.solutionLink": "W:Best_map_editing_practice#Parking_Lots",
+  "131.enabled": true,
+  "131.params": {
+    "titleEN": "Not Massachusetts",
+    "problem": "The segment is assigned to another state",
+    "solutionEN": "Make sure you are editing in MA and change it",
+    "template": "${state}",
+    "regexp": "!/Massachusetts/"
+  },
+  "131.solutionLink": "W:Creating_and_editing_road_segments#Address_Properties",
+  "132.enabled": true,
+  "132.params": {
+    "titleEN": "Wrong name for City or County street",
+    "problemEN": "City or County Hwy Name is not used here",
+    "solutionEN": "Check sources and change the street's name",
+    "template": "${state}:${street}#${altStreet[#]}",
+    "regexp": "/Massachusetts:.*(Cs-|CS-|County Hwy |County Rd |Cr-|Co Rd |CH-|Ch-|Co Rte )/"
+  },
+  "132.solutionLink": "W:Highway_naming/USA",
+  "133.enabled": true,
+  "133.params": {
+    "titleEN": "Bad TTS Street name",
+    "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
+    "solutionEN": "Add a period after Jr or St or Dr where required",
+    "template": "${street}#${altStreet[#]}",
+    "regexp": "/^([SNEW] )+(St |Dr )|^St |^Dr |Jr |Rev /"
+  },
+  "133.solutionLink": "W:Abbreviations_and_acronyms#Standard_suffix_abbreviations",
+  "134.enabled": true,
+  "134.severity": "warning",
+  "134.params": {
+    "titleEN": "Alley not PLR",
+    "problemEN": "Alleys should be set to the PLR Road Type",
+    "solutionEN": "Change Road Type",
+    "template": "${street}:${typeRank}",
+    "regexp": "/.*(Alley| Aly):[^7]/"
+  },
+  "134.solutionLink": "W:Road_types/USA#Parking_Lot_Road",
+  "135.enabled": true,
+  "135.params": {
+    "titleEN": "Incorrect Highway Name",
+    "problemEN": "Massachusetts uses SR for state and US for national highway names",
+    "solutionEN": "Rename the Street or Alt Street",
+    "template": "${state}:${street}#${altStreet[#]}",
+    "regexp": "/Massachusetts:.*(Ma Hwy |State Hwy |MA-|SH-|State Rd |SR=|State Rte |^Rte |U\.?[Ss]\.? [Hh](WY|wy|ighway))/"
+  },
+  "135.solutionLink": "W:Massachusetts",
+  "136.enabled": true,
+  "136.params": {
+    "titleEN": "Wrong road type",
+    "problemEN": "All US Highways should be at least Major Highway (except BUS, SPUR, LOOP)",
+    "solutionEN": "Change the road type to Major Highway",
+    "template": "${typeRank}:#${street}@#${altStreet[@#]}@",
+    "regexp": "/^[1-9][^245]?:.*#(US Hwy |US-)[0-9]+( ALT| BYP| CONN| TRUCK| Scenic| [NSWE])*@/i"
+  },
+  "136.solutionLink": "W:Road_types/USA#Major_Highway",
+  "138.enabled": true,
+  "138.params": {
+    "titleEN": "Invalid Interstate Name",
+    "problemEN": "There seems to be an extra space there",
+    "SolutionEN": "Remove the extra space in the name",
+    "template": "${street}#${altStreet[#]}",
+    "regexp": "/I- /"
+  },
+  "138.solutionLink": "W:Road_names/USA#United_States_Interstate_Highway_System",
+  "139.enabled": true,
+  "139.params": {
+    "titleEN": "City Name on Freeway",
+    "problemEN": "City names on Freeway segments can cause detours",
+    "solutionEN": "Remove the City Name",
+    "template": "${typeRank}:${city}",
+    "regexp": "/15:.+$/"
+  },
+  "139.solutionLink": "W:Massachusetts#City_Names_on_Freeway_Segments",
+  //Freeway lock
+  "150.enabled": true,
+  "150.params": {
+  // {number} minimum lock level
+  "n": 5,
+  },
+  //Major Highway lock
+  "151.enabled": true,
+  "151.params": {
+  // {number} minimum lock level
+  "n": 4,
+  },
+  //Minor Highway lock
+  "152.enabled": true,
+  "152.params": {
+  // {number} minimum lock level
+  "n": 3,
+  },
+  //Ramp lock
+  "153.enabled": true,
+  "153.params": {
+    // {number} minimum lock level
+    "n": 2,
+  },
+  //Primary Street lock
+  "154.enabled": true,
+  "154.params": {
+  // {number} minimum lock level
+  "n": 2,
+  },
+  //Default US checks
+  "27.enabled": !0,
+  "90.enabled": !0,
+  "106.enabled": !0,
+  "112.enabled": !1,
+  "170.enabled": !0,
+  "170.params": {
+      regexp: "/^(?!(to) [^a-z])((S|N|W|E) )?[a-z]/"
+  },
+  "171.enabled": !0,
+  "171.solutionLink": "W:Abbreviations_&_Acronyms#Standard_Suffix_Abbreviations"
+};
