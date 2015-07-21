afc8906a9322e28dc3dee6e32813c4648ae36b7c
xanderb-waze <xander@louisvillehighlands.net>
2015-07-20 21:12:38 -0400

    Update Validator_TN_localization.user.js

--- Validator_TN_localization.user.js
+++ Validator_TN_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Tennessee
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.26
+// @version             1.1.1.27
 // @author              xanderb
 // @description         This script localizes WME Validator for Tennessee, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-07-03",
+  ".updated": "2015-07-20",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -78,7 +78,7 @@ window.WME_Validator_United_States = {
     "problemEN": "CS and Cs are not read correctly by TTS or County Hwy Name is wrong",
     "solutionEN": "Check sources and change the street's name",
     "template": "${state}:${street}#${altStreet[#]}",
-    "regexp": "/Tennessee:.*(Cs-|CS-|County Rd |Cr-|Co Rd|CH-|Ch-)/"
+    "regexp": "/Tennessee:.*(Cs-|CS-|County Rd |Cr-|Co Rd|CH-|Ch-|CR-)/"
   },
   "132.solutionLink": "W:Highway_naming/USA",
   "133.enabled": true,
