d1fc8d3a0d1731a141e413a1bff3e3f04502be2b
xanderb-waze <xander@louisvillehighlands.net>
2015-10-31 14:20:16 -0400

    Update Validator_TN_localization.user.js
    ditched default check #50

--- Validator_TN_localization.user.js
+++ Validator_TN_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Tennessee
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.31
+// @version             1.1.1.32
 // @author              xanderb
 // @description         This script localizes WME Validator for Tennessee, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -154,7 +154,6 @@ window.WME_Validator_United_States = {
   "39.enabled": !0,
   "43.enabled": !0,
   "48.enabled": !0,
-  "50.enabled": !0,
   "78.enabled": !0,
   "87.enabled": !0,
   "90.enabled": !0,
