821401b1d29e014202ca04f9df101f328f78f4e6
xanderb-waze <xander@louisvillehighlands.net>
2015-10-31 12:06:10 -0400

    Update Validator_PA_Localization.user.js
    default check update

--- Validator_PA_Localization.user.js
+++ Validator_PA_Localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Pennsylvania
 // @namespace           https://greasyfork.org/en/users/15052
-// @version             1.1.32
+// @version             1.1.33
 // @author              Tythesly and xanderb
 // @credit              Credit to xanderb for the base template
 // @description         This script localizes WME Validator for Pennsylvania. You also need main package (WME Validator) installed.
@@ -36,7 +36,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "tythesly",
-  ".updated": "2015-10-26",
+  ".updated": "2015-10-31",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -134,17 +134,32 @@ window.WME_Validator_United_States = {
   },
   //Default US checks
   "24.enabled": !0,
+  "25.enabled": !0,
   "27.enabled": !0,
   "34.enabled": !0,
+  "35.enabled": !0,
   "38.enabled": !0,
   "39.enabled": !0,
+  "43.enabled": !0,
+  "48.enabled": !0,
+  "50.enabled": !0,
+  "78.enabled": !0,
+  "87.enabled": !0,
   "90.enabled": !0,
   "106.enabled": !0,
+  "107.enabled": !0,
+  "108.enabled": !0,
   "109.enabled": !0,
   "112.enabled": !1,
+  "118.enabled": !0,
+  "116.enabled": !0,
+  "119.enabled": !0,
   "120.enabled": !0,
   "121.enabled": !0,
+  "172.enabled": !0,
   "173.enabled": !0,
+  "190.enabled": !0,
+  "192.enabled": !0,
   "170.enabled": !0,
   "170.params": {
       regexp: "/^(?!(to) [^a-z])((S|N|W|E) )?[a-z]/"
