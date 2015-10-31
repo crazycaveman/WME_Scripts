9a4e1fcc05515e2cdc06187dbad142e7198962ea
xanderb-waze <xander@louisvillehighlands.net>
2015-10-31 14:13:11 -0400

    Update Validator_ILL_localization.user.js
    ditched default check #50

--- Validator_ILL_localization.user.js
+++ Validator_ILL_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Illinois
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.5
+// @version             1.1.6
 // @author              xanderb
 // @description         This script localizes WME Validator for Illinois, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -139,7 +139,6 @@ window.WME_Validator_United_States = {
   "39.enabled": !0,
   "43.enabled": !0,
   "48.enabled": !0,
-  "50.enabled": !0,
   "78.enabled": !0,
   "87.enabled": !0,
   "90.enabled": !0,
