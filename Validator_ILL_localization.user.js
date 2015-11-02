6b1e7f063e1b1c96e47b6cb7966199f1de9509ed
xanderb-waze <xander@louisvillehighlands.net>
2015-11-02 15:06:33 -0500

    Update Validator_ILL_localization.user.js
    TTS Bug Fix

--- Validator_ILL_localization.user.js
+++ Validator_ILL_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Illinois
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.6
+// @version             1.1.7
 // @author              xanderb
 // @description         This script localizes WME Validator for Illinois, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -41,9 +41,9 @@ window.WME_Validator_United_States = {
   "128.params": {
     "titleEN": "Bad TTS Street name",
     "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
-    "solutionEN": "Add a period after St or Dr at the beginning of the street name",
+    "solutionEN": "Add a period after Jr, St, Dr, or Rev where required",
     "template": "${street}#${altStreet[#]}",
-    "regexp": "/((^|#|(\\/|[NEWS]|Rue|Place)\\s)(St|Dr)|(Jr|Rev)) /"
+    "regexp": "/((^([SNEW] )?(St|Dr))|Jr|Rev) /"
   },
   "128.solutionLink": "W:Abbreviations_and_acronyms#Standard_suffix_abbreviations",
   "129.enabled": true,
