080d51b660bf4d74187c0ca4e90c85eb187f1201
xanderb-waze <xander@louisvillehighlands.net>
2015-11-02 18:09:32 -0500

    Update Validator_WV_localization.user.js
    space

--- Validator_WV_localization.user.js
+++ Validator_WV_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for West Virginia
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.16
+// @version             1.1.1.17
 // @author              xanderb
 // @description         This script localizes WME Validator for West Virginia, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -70,7 +70,7 @@ window.WME_Validator_United_States = {
     "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
     "solutionEN": "Add a period after Jr, St, Dr, or Rev where required",
     "template": "${street}#${altStreet[#]}",
-    "regexp": "/((^| )(St|Dr)(?! ([NEWS][EW]?\\b|\/|\\())|(Jr|Rev))/"
+    "regexp": "/((^| )(St|Dr)(?! ([NEWS][EW]?\\b|\/|\\())|(Jr|Rev)) /"
   },
   "131.solutionLink": "W:Abbreviations_and_acronyms/USA#Standard_suffix_abbreviations",
   "132.enabled": true,
