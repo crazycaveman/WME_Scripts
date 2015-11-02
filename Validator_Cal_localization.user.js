ba5657df7cfe7b608dc8b740b53d2ae3830ad188
xanderb-waze <xander@louisvillehighlands.net>
2015-11-02 17:56:21 -0500

    Update Validator_Cal_localization.user.js
    space

--- Validator_Cal_localization.user.js
+++ Validator_Cal_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for California
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.7
+// @version             1.1.8
 // @author              xanderb
 // @description         This script localizes WME Validator for California, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-10-31",
+  ".updated": "2015-11-02",
   ".link": "TODO: ",
   "130.enabled": true,
   "130.params": {
@@ -52,7 +52,7 @@ window.WME_Validator_United_States = {
     "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
     "solutionEN": "Add a period after Jr, St, Dr, or Rev where required",
     "template": "${street}#${altStreet[#]}",
-    "regexp": "/((^| )(St|Dr)(?! ([NEWS][EW]?\\b|\/|\\())|(Jr|Rev))/"
+    "regexp": "/((^| )(St|Dr)(?! ([NEWS][EW]?\\b|\/|\\())|(Jr|Rev)) /"
   },
   "131.solutionLink": "W:Abbreviations_and_acronyms#Standard_suffix_abbreviations/USA",
   "132.enabled": true,
