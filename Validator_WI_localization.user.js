78d2f9c7cf01952b0e56bbe2e6d4c67abcf4c196
xanderb-waze <xander@louisvillehighlands.net>
2015-11-02 18:08:50 -0500

    Update Validator_WI_localization.user.js
    space

--- Validator_WI_localization.user.js
+++ Validator_WI_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Wisconsin
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.6
+// @version             1.1.7
 // @author              xanderb
 // @description         This script localizes WME Validator for Wisconsin, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -43,7 +43,7 @@ window.WME_Validator_United_States = {
     "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
     "solutionEN": "Add a period after Jr, St, Dr, or Rev where required",
     "template": "${street}#${altStreet[#]}",
-    "regexp": "/((^| )(St|Dr)(?! ([NEWS][EW]?\\b|\/|\\())|(Jr|Rev))/"
+    "regexp": "/((^| )(St|Dr)(?! ([NEWS][EW]?\\b|\/|\\())|(Jr|Rev)) /"
   },
   "128.solutionLink": "W:Abbreviations_and_acronyms/USA",
   "129.enabled": true,
