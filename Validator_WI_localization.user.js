90f2043f8326aef7a11419e764cc1277a9cc91f2
xanderb-waze <xander@louisvillehighlands.net>
2015-11-02 15:32:29 -0500

    Update Validator_WI_localization.user.js
    TTS Fix

--- Validator_WI_localization.user.js
+++ Validator_WI_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Wisconsin
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.5
+// @version             1.1.6
 // @author              xanderb
 // @description         This script localizes WME Validator for Wisconsin, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,17 +35,17 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-10-31",
+  ".updated": "2015-11-02",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
     "titleEN": "Bad TTS Street name",
     "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
-    "solutionEN": "Add a period after Jr or St or Dr where required",
+    "solutionEN": "Add a period after Jr, St, Dr, or Rev where required",
     "template": "${street}#${altStreet[#]}",
-    "regexp": "/^([SNEW] )+(St |Dr )|^St |^Dr |Jr |Rev /"
+    "regexp": "/((^| )(St|Dr)(?! ([NEWS][EW]?\\b|\/|\\())|(Jr|Rev))/"
   },
-  "128.solutionLink": "W:Abbreviations_and_acronyms#Standard_suffix_abbreviations",
+  "128.solutionLink": "W:Abbreviations_and_acronyms/USA",
   "129.enabled": true,
   "129.params": {
     "titleEN": "Incorrect Highway",
