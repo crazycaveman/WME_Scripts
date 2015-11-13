0e0fefe17d4e8acbc4c1fe4d4b6d0aadddd4f1c2
xanderb-waze <xander@louisvillehighlands.net>
2015-11-13 13:28:18 -0500

    Update Validator_AU_localization.user.js
    138 update

--- Validator_AU_localization.user.js
+++ Validator_AU_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Australia
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.17
+// @version             1.1.18
 // @author              xanderb
 // @description         This script localizes WME Validator for Australia. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_Australia = {
   ".country": "Australia",
   ".codeISO": "AU",
   ".author": "xanderb",
-  ".updated": "2015-11-12",
+  ".updated": "2015-11-13",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -70,7 +70,7 @@ window.WME_Validator_Australia = {
     "problemEN": "Streets that start with St and Dr result in TTS reading Street or Drive",
     "solutionEN": "Add a period after Jr, St, Dr, or Rev where required",
     "template": "${street}#${altStreet[#]}",
-    "regexp": "/((^| )(St|Dr)(\.$)?(?! ((CONN|Ext|[NEWS][EW]?)\\b|\/|\\())|(Jr|Rev)) /"
+    "regexp": "/((^| )(St|Dr)(\.$)?(?! ((CONN|Ext|East|West|North|South|[NEWS][EW]?)\\b|\/|\\())|(Jr|Rev)) /"
   },
   "138.solutionLink": "W:Abbreviations_and_acronyms#Standard_suffix_abbreviations",
   "139.enabled": true,
