05fc8bb3538349a9a02b1a54c7452d03da2ea458
xanderb-waze <xander@louisvillehighlands.net>
2015-11-25 22:40:53 -0500

    Update Validator_OH_localization.user.js
    County Hwy consolidation and added "CR "

--- Validator_OH_localization.user.js
+++ Validator_OH_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Ohio
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.9
+// @version             1.1.10
 // @author              xanderb
 // @description         This script localizes WME Validator for Ohio, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-11-03",
+  ".updated": "2015-11-25",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -78,7 +78,7 @@ window.WME_Validator_United_States = {
     "problemEN": "CS and Cs are not read correctly by TTS or County Hwy Name is wrong",
     "solutionEN": "Check sources and change the street's name",
     "template": "${state}:${street}#${altStreet[#]}",
-    "regexp": "/Ohio:.*(Cs-|CS-|County Hwy |County Rd |Cr-|Co Rd|Ch-|T-|C-|Co Hwy |CH-|Twp Hwy |TH-)/"
+    "regexp": "/Ohio:.*(C[sSrHh]?(-|R? )|Co(unty)? (Hwy|Rd) |Twp Hwy |TH?-)/"
   },
   "132.solutionLink": "W:Highway_naming/USA",
   "133.enabled": true,
