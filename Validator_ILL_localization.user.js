96452523476e1460a72329bd9c362ab0f448e481
xanderb-waze <xander@louisvillehighlands.net>
2016-01-03 14:50:44 -0500

    Update Validator_ILL_localization.user.js
    County road search

--- Validator_ILL_localization.user.js
+++ Validator_ILL_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Illinois
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.13
+// @version             1.1.14
 // @author              xanderb
 // @description         This script localizes WME Validator for Illinois, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2015-11-18",
+  ".updated": "2016-01-03",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -79,7 +79,7 @@ window.WME_Validator_United_States = {
     "problemEN": "CS and Cs are not read correctly by TTS or County Hwy Name is wrong",
     "solutionEN": "Check sources and change the street's name",
     "template": "${state}:${street}#${altStreet[#]}",
-    "regexp": "/Illinois:.*(Cs-|CS-|County Hwy|County Rd|Cr-|Co Rd|Ch-|CH-)/"
+    "regexp": /Illinois:.*(Co(unty)? (H(ighway|wy)|R(oa)?d)|C[HhSsr]-)/"
   },
   "132.solutionLink": "W:Highway_naming/USA",
   "133.enabled": true,
