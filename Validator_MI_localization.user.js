8264572234058200cbbd37a1d863e47347eb7569
xanderb-waze <xander@louisvillehighlands.net>
2016-03-20 21:43:43 -0400

    Update Validator_MI_localization.user.js

--- Validator_MI_localization.user.js
+++ Validator_MI_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for Michigan
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.14
+// @version             1.1.15
 // @author              xanderb
 // @description         This script localizes WME Validator for Michigan, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -133,7 +133,7 @@ window.WME_Validator_United_States = {
   "153.enabled": true,
   "153.params": {
     // {number} minimum lock level
-    "n": 4,
+    "n": 3,
   },
   //Primary Street lock
   "154.enabled": true,
