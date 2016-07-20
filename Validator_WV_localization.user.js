a211e4c04fe1a4832cfdffacf38fa090a90160c4
xanderb-waze <xander@louisvillehighlands.net>
2016-07-20 09:09:03 -0400

    Update Validator_WV_localization.user.js
    locks

--- Validator_WV_localization.user.js
+++ Validator_WV_localization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                WME Validator Localization for West Virginia
 // @namespace           https://greasyfork.org/en/users/9555
-// @version             1.1.1.36
+// @version             1.1.1.37
 // @author              xanderb
 // @description         This script localizes WME Validator for West Virginia, USA. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
@@ -35,7 +35,7 @@ window.WME_Validator_United_States = {
   ".country": "United States",
   ".codeISO": "US",
   ".author": "xanderb",
-  ".updated": "2016-07-18",
+  ".updated": "2016-07-20",
   ".link": "TODO: ",
   "128.enabled": true,
   "128.params": {
@@ -155,11 +155,17 @@ window.WME_Validator_United_States = {
   "151.enabled": true,
   "151.params": {
   // {number} minimum lock level
-  "n": 3,
+  "n": 4,
   },
   //Minor Highway lock
   "152.enabled": true,
   "152.params": {
+  // {number} minimum lock level
+  "n": 3,
+  },
+  //Primary Street lock
+  "154.enabled": true,
+  "154.params": {
   // {number} minimum lock level
   "n": 2,
   },
