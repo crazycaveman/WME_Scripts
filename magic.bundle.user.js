962f5be24a3701fa68419c05cece0bbff23424e8
Joe <crazycaveman@users.noreply.github.com>
2018-05-14 13:28:18 -0400

    Add userscript metadata for WME Magic
    Make it easier to install WME Magic

---
+++ magic.bundle.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Waze Map Editor - Magic
+// @namespace    https://waze.xpto.org/
+// @version      0.0.0
+// @description  show on WME what should be fixed
+// @author       Delfim Machado - dbcm@xpto.org
+// @match        https://beta.waze.com/*editor*
+// @match        https://www.waze.com/*editor*
+// @exclude      https://www.waze.com/*user/*editor/*
+// @grant        none
+// @icon         https://waze.xpto.org/magic/logo.png
+// @downloadURL  https://waze.xpto.org/magic/magic.bundle.user.js
+// ==/UserScript==
